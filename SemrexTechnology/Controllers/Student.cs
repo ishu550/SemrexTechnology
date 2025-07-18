413a65d4ad572673342c5c544c4111d80af6b7b7
KKhushbu <khushbukushwah27@gmail.com>
2025-07-18 10:51:46 +0530

    Model changes successfully

---
+++ SemrexTechnology/Controllers/Student.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SemrexTechnology.Controllers
+{
+    public class Student : Controller
+    {
+        public IActionResult Index()
+        {
+            //Hello Controller 
+            //Code changed by Khushbu Kushwah
+            return View();
+        }
+    }
+}
